f190c3a7919665aaa036391c05d60e4bb51e7b19
nathalie loussouarn <natel@hotmail.com>
2022-04-26 21:41:18 +0200

    comment limitation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,17 +10,14 @@ function editNav() {
 // DOM Elements
 const formData = document.querySelectorAll(".formData");
 
-
-// definition of the function that opens or closes the modal
+// function that opens or closes the modal
 toggleModal = () => {
-  // modal element collect
   const modalContainer = document.querySelector(".bground");
-  // adding or removing the active class to the modal element
   modalContainer.classList.toggle("active");
 }
 
 // collect of all trigger element & iteration
-document.querySelectorAll(".modal-trigger").forEach(btn => 
-  // on each element listen click and call of the function
-  btn.addEventListener("click", toggleModal)
+document.querySelectorAll(".modal-trigger")
+  .forEach(
+    btn => btn.addEventListener("click", toggleModal)
 );
