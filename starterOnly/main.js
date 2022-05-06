195d0c174123db8be27588f9c4d3c1f9cd476978
nathalie loussouarn <natel@hotmail.com>
2022-05-06 19:01:22 +0200

    add checkbox an disabled submit function, code cleaning

--- starterOnly/main.js
+++ starterOnly/main.js
@@ -1,142 +1,156 @@
-// MODAL //
+// VARIABLES
+const modalTriggers = document.querySelectorAll(".modal-trigger");
 
-// function that opens or closes the modal
+const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("form");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const radios = document.querySelector('input[type="radio"]');
+const checkbox = document.getElementById("checkbox1");
+const submit = document.querySelector('input[type="submit"]');
+
+// function open or close the modal
 toggleModal = () => {
   const modalContainer = document.querySelector(".bground");
   modalContainer.classList.toggle("active");
 }
 
-// collect of all trigger element & iteration
-document.querySelectorAll(".modal-trigger")
-  .forEach(
-    btn => btn.addEventListener("click", toggleModal)
-);
-
-// FORM //
-
-const formData = document.querySelectorAll(".formData");
-
-// collect all inputs & stock on variable
-const form = document.getElementById("form");
-const firstname = document.getElementById("firstname");
-const lastname = document.getElementById("lastname");
-const email = document.getElementById("email");
-const birthday = document.getElementById("birthday");
-const quantity = document.getElementById("quantity");
-
-// firstname input
+// first name input validation function
 isFirstnameValid = () => {
-  let validFirstname = false;
-  if (firstname === "") {
-    console.log("missing something");
-  } else if (/[A-Za-z0-9]{2,}/.test(firstname.value.trim())) {
-    console.log("ok");
+  firstname.classList.remove(":valid");
+  const regex = /[A-Za-z0-9]{2,}/;
+  if (regex.test(firstname.value.trim())) {
     firstname.classList.add(":valid");
-    validFirstname = true;
-  } else {
-    console.log("nop");
-    firstname.classList.remove(":valid");
+console.log("ok mamie");
+
+    return true;
+  }
+
+  return false
+}
+
+//  last name input validation function
+isLastnameValid = () => {
+  lastname.classList.remove(":valid");
+  const regex = /[A-Za-z0-9]{2,}/;
+  if (regex.test(lastname.value.trim())) {
+    lastname.classList.add(":valid");
+console.log("ok paulo");
+
+    return true;
+  }
+
+  return false
+}
+
+// email input validation function
+isEmailValid = () => {
+  email.classList.remove(":valid");
+  const regex = /[A-Za-z0-9]{1,}@[A-Za-z0-9]{2,}.[A-Za-z0-9]{2,}/;
+  if (regex.test(email.value.trim())) {
+    email.classList.add(":valid");
+console.log("ok mail mail");
+
+    return true;
+  }
+
+  return false;
+}
+
+// birthday input validation fucntion
+isBirthdateValid = () => {
+  birthdate.classList.remove(":valid");
+  const regex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+  if (regex.test(birthdate.value)) {
+    birthdate.classList.add(":valid");
+console.log("ok");
+
+    return true;
+  }
+
+  return false;
+}
+
+// quantity input validation function
+isQuantityValid = () => {
+  quantity.classList.remove(":valid");
+  const regex = /\d/;
+  if (regex.test(quantity.value.trim())) {
+    quantity.classList.add(":valid");
+console.log("ok");
+
+    return true;
+  }
+
+  return false
+}
+
+// radio button validation function
+atLeastOneRadioButtonIsChecked = () => {
+  for (var i=0; i<radios.length; i++) {
+    if (radios[i].checked){
+console.log("ok radio");
+
+      return true;
+    }  
+  }
+
+  return false
+}
+
+// checkbox validation function
+isCheckboxChecked = () => {
+  if (checkbox.checked){
+console.log("ok checkbox");
+
+      return true;
+    }  
+
+  return false
+}
+
+// disabled submit button function
+disabledSubmit = () => {
+  submit.setAttribute("disabled", true);
+console.log("button désactivé");
+  if (isFirstnameValid && isLastnameValid && isEmailValid) {
+    submit.removeAttribute("disabled");
+console.log("button activé");
   }
 }
+
+// form validation function
+validateForm = () => {
+  e.preventDefault();
+
+  let validFirstname = isFirstnameValid;
+  let validLastname = isLastnameValid;
+  let validEmail = isEmailValid;
+  let validBirthdate = isBirthdateValid;
+  let validQuantity = isQuantityValid;
+  let validRadio = atLeastOneRadioButtonIsChecked;
+  let validCheckbox = isCheckboxChecked;
+
+  if (validFirstname === true && validLastname === true && validEmail === true && validBirthdate === true 
+    && validQuantity === true && validRadio === true && validCheckbox=== true ) {
+
+    return true;
+  }
+
+  return false;
+}
+
+// EVENT LISTENERS
+modalTriggers.forEach(btn => btn.addEventListener("click", toggleModal));
 
 firstname.addEventListener("blur", isFirstnameValid);
-
-//  lastname input
-isLastnameValid = () => {
-  let validLastname = false;
-  if (lastname === "") {
-    console.log("missing something");
-  } else if (/[A-Za-z0-9]{2,}/.test(lastname.value.trim())) {
-    console.log("ok");
-    lastname.classList.add(":valid");
-    validLastname = true;
-  } else {
-    console.log("nop");
-    lastname.classList.remove(":valid");
-  }
-}
-
 lastname.addEventListener("blur", isLastnameValid);
-
-// email input
-isEmailValid = () => {
-  let validEmail = false;
-  if (email === "") {
-    console.log("missing something");
-  } else if (/[A-Za-z0-9]{1,}@[A-Za-z0-9]{2,}.[A-Za-z0-9]{2,}/.test(email.value.trim())) {
-    console.log("ok");
-    email.classList.add(":valid");
-    validEmail = true;
-  } else {
-    console.log("nop");
-    email.classList.remove(":valid");
-  }
-}
-
 email.addEventListener("blur", isEmailValid);
-
-// birthday input 
-isBirthdayValid = () => {
-  let validBirthday = false;
-  if (birthday === "") {
-    console.log("missing something");
-  } else if (/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/.test(birthday.value)) {
-    console.log("ok");
-    birthday.classList.add(":valid");
-    validBirthday = true;
-  } else {
-    console.log("nop");
-    birthday.classList.remove(":valid");
-  }
-}
-
-birthday.addEventListener("change", isBirthdayValid);
-
-// quantity input 
-isQuantityValid = () => {
-  let validQuantity = false;
-  if (quantity === "") {
-    console.log("missing something");
-  } else if (/\d/.test(quantity.value.trim())) {
-    console.log("ok");
-    quantity.classList.add(":valid");
-    validQuantity = true;
-  } else {
-    console.log("nop");
-    quantity.classList.remove(":valid");
-  }
-}
-
+birthdate.addEventListener("change", isBirthdateValid);
 quantity.addEventListener("blur", isQuantityValid);
+formData.forEach(input => input.addEventListener("change", disabledSubmit));
+form.addEventListener("submit", atLeastOneRadioButtonIsChecked, isCheckboxChecked, validateForm);
 
-// radio button
-document.querySelector(".button")
-  .addEventListener("change", (e) => {
-  const radios = document.querySelector('input[name="location"]');
-  const radioValue = false;
-
-  for (var i=0; i<radios.length; i++) {
-    if (radios[i].checked == true){
-      radioValue = true;    
-      console.log("ok");
-    }
-  }
-});
-
-
-// form validation
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  validateForm();
-});
-
-validateForm = () => {
-  const submit = document.getElementsByClassName("btn-submit");
-  if (validFirstname === true) {
-    submit.classList.removeAttribute("disabled");
-    submit.classList.add(":valid");
-    return true;
-  } else {
-    console.log("try again !");
-  }
-}
